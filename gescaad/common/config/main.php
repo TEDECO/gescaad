df619321d478e5558b763522b3fdbcd1a30be15b
Juan M.Victoria <dreamlord@gmail.com>
2018-06-23 14:17:01 +0200

    Fix.

--- gescaad/common/config/main.php
+++ gescaad/common/config/main.php
@@ -14,8 +14,8 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-		'authManager' => [
-            'class' => 'yii\rbac\DbManager,
-		]
+    'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ]
     ],
 ];
