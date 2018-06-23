0891cd185c3441593f20ddae6be14f31134d12cc
Juan M.Victoria <dreamlord@gmail.com>
2018-06-23 14:12:00 +0200

    1st DB configurarion and migration preparatives.

---
+++ gescaad/common/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=gescaad',
+            'username' => 'adm_gescaad',
+            'password' => 'xKw1S8sq@Lyuttj',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
