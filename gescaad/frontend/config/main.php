b07beb92d879061cdeb98fe15d2b7ba5aaa62646
Juan M.Victoria <dreamlord@gmail.com>
2018-07-04 20:13:14 +0200

    Added languaje in frontend/config/main.

--- gescaad/frontend/config/main.php
+++ gescaad/frontend/config/main.php
@@ -8,6 +8,8 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'GESCAAD',
+    'language'=> 'es',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
